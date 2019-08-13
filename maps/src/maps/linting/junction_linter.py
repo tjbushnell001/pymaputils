4775c4db6c3c7f505ebec7a300de284729da6d65
Christian <cjwawrzonek@gmail.com>
2019-08-13 09:19:30 -0700

    last david comments

--- maps/src/maps/linting/junction_linter.py
+++ maps/src/maps/linting/junction_linter.py
@@ -20,31 +20,33 @@ def lint_junction(junction, lane_map, issue_layer):
     if len(outflows) > 1:
         transition_count = {"MERGE": 0, "SPLIT": 0, "UNKNOWN": 0}
         for lane in outflows:
-            if lane.properties['from_split'] is True:
-                if lane.properties['lane_transition_type'] is None:
-                    transition_type = "UNKNOWN"
-                else:
-                    transition_type = lane.properties['lane_transition_type']
-                transition_count[transition_type] += 1
+            if lane.properties['lane_transition_type'] is None:
+                transition_type = "UNKNOWN"
+            else:
+                transition_type = lane.properties['lane_transition_type']
+            transition_count[transition_type] += 1
+            if lane.properties['from_split'] is False:
+                issue_layer.add_issue(junction, Issue(IssueType.FROM_SPLIT_MISSING_PROPERTY.name))
 
         if transition_count["UNKNOWN"] == 0:
-            issue_layer.add_issue(junction, Issue(IssueType.POST_TRANSITION_NO_NORMAL.name))
+            issue_layer.add_issue(junction, Issue(IssueType.SPLIT_NO_NORMAL.name))
         elif transition_count["UNKNOWN"] > 1:
-            issue_layer.add_issue(junction, Issue(IssueType.POST_TRANSITION_MULTIPLE_NORMAL.name))
+            issue_layer.add_issue(junction, Issue(IssueType.SPLIT_MULTIPLE_NORMAL.name))
 
     if len(inflows) > 1:
         transition_count = {"MERGE": 0, "SPLIT": 0, "UNKNOWN": 0}
         for lane in inflows:
-            if lane.properties['merging'] is True:
-                if lane.properties['lane_transition_type'] is None:
-                    transition_type = "UNKNOWN"
-                else:
-                    transition_type = lane.properties['lane_transition_type']
-                transition_count[transition_type] += 1
+            if lane.properties['lane_transition_type'] is None:
+                transition_type = "UNKNOWN"
+            else:
+                transition_type = lane.properties['lane_transition_type']
+            transition_count[transition_type] += 1
+            if lane.properties['merging'] is False:
+                issue_layer.add_issue(junction, Issue(IssueType.MERGING_MISSING_PROPERTY.name))
 
         if transition_count["UNKNOWN"] == 0:
-            issue_layer.add_issue(junction, Issue(IssueType.PRE_TRANSITION_NO_NORMAL.name))
+            issue_layer.add_issue(junction, Issue(IssueType.MERGE_NO_NORMAL.name))
         elif transition_count["UNKNOWN"] > 1:
-            issue_layer.add_issue(junction, Issue(IssueType.PRE_TRANSITION_MULTIPLE_NORMAL.name))
+            issue_layer.add_issue(junction, Issue(IssueType.MERGE_MULTIPLE_NORMAL.name))
 
     return issue_layer
