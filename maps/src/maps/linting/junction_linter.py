9ad9923d3e177435e98585bbaccc3b38b27fd7c5
Christian <cjwawrzonek@gmail.com>
2019-08-13 09:29:43 -0700

    pylint

--- maps/src/maps/linting/junction_linter.py
+++ maps/src/maps/linting/junction_linter.py
@@ -1,11 +1,20 @@
 #! /usr/bin/env python
-"""  """
+""" Library to lint map junctions for "Issues" """
 from maps.issue_types import IssueType
-from maps.issues import Issue
+from maps.issues import Issue, IssueLayer
 
 
-def lint_junction(junction, lane_map, issue_layer):
-    # Extract lanes
+def lint_junction(junction, lane_map, issue_layer=None):
+    """
+    Lint a junction, checking the inflow and outflow lanes for the correct properties and formatting.
+
+    :param junction: a geojson junction object to lint
+    :param lane_map: a geojson based tiled map layer
+    :param issue_layer: an optional pre initialized issue layer to write new issues to
+    :return: the issue layer with any new issues added.
+    """
+    issue_layer = IssueLayer() if issue_layer is None else issue_layer
+
     inflows = []
     outflows = []
     for lane_ref in junction.properties['inflow_refs']:
@@ -48,5 +57,8 @@ def lint_junction(junction, lane_map, issue_layer):
             issue_layer.add_issue(junction, Issue(IssueType.MERGE_NO_NORMAL.name))
         elif transition_count["UNKNOWN"] > 1:
             issue_layer.add_issue(junction, Issue(IssueType.MERGE_MULTIPLE_NORMAL.name))
+
+    if len(inflows) > 1 and len(outflows) > 1:
+        issue_layer.add_issue(junction, Issue(IssueType.MULTIPLE_INFLOW_OUTFLOWS.name))
 
     return issue_layer
--- maps/src/maps/linting/lane_linter.py
+++ maps/src/maps/linting/lane_linter.py
@@ -1,13 +1,20 @@
-
 #! /usr/bin/env python
-"""  """
+""" Library to lint map lanes for "Issues" """
 from maps.issue_types import IssueType
-from maps.issues import Issue, IssueLevel
+from maps.issues import Issue, IssueLayer, IssueLevel
 
 TRANSITION_KEYS = ['from_split', 'upcoming_split', 'merged', 'merging']
 
 
-def lint_lane(lane, issue_layer):
+def lint_lane(lane, issue_layer=None):
+    """
+    Lint a lane, checking transitioning properties, warning if there are more than one property true at a time.
+
+    :param lane: a geojson lane object to lint
+    :param issue_layer: an optional pre initialized issue layer to write new issues to
+    :return: the issue layer with any new issues added.
+    """
+    issue_layer = IssueLayer() if issue_layer is None else issue_layer
     transition_props = [1 for k in TRANSITION_KEYS if lane.properties[k]]
     valid = len(transition_props) <= 1
 
