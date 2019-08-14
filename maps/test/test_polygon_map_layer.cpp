767f70c6307be19b3d1b7dac1619606856d10fb5
David Watson <david@embarktrucks.com>
2019-08-13 18:49:44 -0700

    Added a test case for no overtake zones.

--- maps/test/test_polygon_map_layer.cpp
+++ maps/test/test_polygon_map_layer.cpp
@@ -12,7 +12,7 @@ const lane_map::PolygonFeature* findFirstMatch(const BoostPoint& point,
   return nullptr;
 }
 
-TEST(MapTileLoader, polygonLayersBasics)
+TEST(MapTileLoader, localizationZones)
 {
   
   maps::MapLayers map(maps::createMapLayers({maps::MapLayerType::LOCALIZATION_ZONES}));
@@ -35,6 +35,31 @@ TEST(MapTileLoader, polygonLayersBasics)
   auto fov_zone = findFirstMatch(BoostPoint(37.73488314788311, -122.40448594093323), features);
   EXPECT_NE(fov_zone, nullptr);
   EXPECT_EQ(fov_zone->properties["type"], "limit_fov");
+}
+
+TEST(MapTileLoader, noOvertakeLaneAnnotations)
+{
+  
+  maps::MapLayers map(maps::createMapLayers({maps::MapLayerType::LANE_ANNOTATION}));
+  // get layer
+  auto layer = map.getLayerAs<maps::PolygonFeatureMapLayer>(maps::MapLayerType::LANE_ANNOTATION, "no_passing_zones");
+  EXPECT_NE(layer, nullptr);
+
+  EXPECT_NE(layer->getFeatures(), nullptr);
+  const std::vector<lane_map::PolygonFeature>& features = *layer->getFeatures();
+
+  EXPECT_EQ(features.size(), 17);
+
+  auto no_zone = findFirstMatch(BoostPoint(33.908035299893015, -116.52236938476562), features);
+  EXPECT_EQ(no_zone, nullptr);
+
+  auto west_zone = findFirstMatch(BoostPoint(33.902336404480685, -116.57180786132812), features);
+  EXPECT_NE(west_zone, nullptr);
+  EXPECT_EQ(west_zone->properties["route"], "texas-ontario");
+
+  auto east_zone = findFirstMatch(BoostPoint(33.696922692957685, -116.09664916992189), features);
+  EXPECT_NE(east_zone, nullptr);
+  EXPECT_EQ(east_zone->properties["route"], "ontario-texas");
 }
 
 int main(int argc, char** argv)
