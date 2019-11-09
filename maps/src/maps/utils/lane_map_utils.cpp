74010ca86343257e42bf7de93558023ad97c7752
Christian <cjwawrzonek@gmail.com>
2019-11-08 16:02:28 -0800

    reduce is_merging distance to be within submap_radius

--- maps/src/maps/utils/lane_map_utils.cpp
+++ maps/src/maps/utils/lane_map_utils.cpp
@@ -165,14 +165,10 @@ BoundaryRef getLeftBoundary(const Lane& lane)
 }
 
 
-std::vector<geometry_msgs::Point> getLanePtsForward(const maps::LaneSubMap& map,
-                                                    const LaneRef& lane_ref, bool debug)
+std::vector<geometry_msgs::Point> getLanePtsForward(const maps::LaneSubMap& map, const LaneRef& lane_ref)
 {
   const Lane* lane = map.getLane(lane_ref);
   if (!lane) {
-    if (debug) {
-        std::cout << "HERE!?" << std::endl;
-    }
     return {};
   }
   return getLanePtsForward(*lane);
