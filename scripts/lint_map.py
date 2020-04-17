a68fbd9a632e089d8fa63c3beaaca39f8447e8d2
Christian <cjwawrzonek@gmail.com>
2020-04-17 13:21:34 -0700

    make linter work outside embite

--- scripts/lint_map.py
+++ scripts/lint_map.py
@@ -3,16 +3,14 @@
 import argparse
 import os
 import glob
-import rospkg
 
 from maps.linting import route_linter
 from maps.utils import emblog
 emblog.set_ros(False)
 
-parser = argparse.ArgumentParser("Lint all da lanes")
+parser = argparse.ArgumentParser("Lint all da maps")
 parser.add_argument("--route_id", action='append', dest="route_ids", help="a list of routes to lint")
 parser.add_argument("--map_dir", default=None, help="input dir for tiles, default to lane_map_server")
-parser.add_argument("--map_reader_dir", default=None, help="dir of map reader files")
 parser.add_argument("--out_file", default=None, help="file to write issue layer output")
 parser.add_argument("--issue_type", dest="issue_types", action='append', default=None,
                     help="list of issue types to search for")
@@ -23,20 +21,13 @@ def main():
 
     map_dir = args.map_dir
     if map_dir is None:
-        map_dir = os.path.join(rospkg.RosPack().get_path('lane_map_server'), 'maps/tiled_maps/usa')
+        map_dir = os.path.join(os.path.expanduser('~'), 'src/tiled_maps/usa')
 
-    map_reader_dir = args.map_reader_dir
-    if map_reader_dir is None:
-        map_reader_dir = os.path.join(rospkg.RosPack().get_path('map_reader'), 'maps')
-
-    routes_dir = os.path.join(rospkg.RosPack().get_path('brain'), 'params/routes')
-    route_ids = args.route_ids
-    if route_ids is None:
-        route_ids = []
-        # demap_dirfault to all routes
-        for fn in glob.glob(os.path.join(routes_dir, '*.yaml')):
-            route_id = os.path.splitext(os.path.basename(fn))[0]
-            route_ids.append(route_id)
+    map_reader_dir = os.path.join(map_dir, 'map_reader')
+    route_ids = set()
+    for fn in glob.glob(os.path.join(map_reader_dir, '*.json')):
+        route_id = os.path.splitext(os.path.basename(fn))[0]
+        route_ids.add(route_id)
 
     issue_layer, failures = route_linter.lint_routes(map_dir, map_reader_dir, route_ids, args.issue_types)
 
