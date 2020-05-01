e40d8aad101ccacb774b2f0bf8acf0fba380679e
Jared Jensen <jared@embarktrucks.com>
2020-04-30 21:56:19 -0700

    Add the ability back in to pass in a list of route ids

--- scripts/lint_map.py
+++ scripts/lint_map.py
@@ -14,7 +14,7 @@ from maps.utils import emblog
 emblog.set_ros(False)
 
 parser = argparse.ArgumentParser("Lint all da maps")
-parser.add_argument("--route_id", action='append', dest="route_ids", help="a list of routes to lint")
+parser.add_argument("--route_ids", nargs='+', help="a list of routes to lint")
 parser.add_argument("--map_dir", default=None, help="input dir for tiles, default to lane_map_server")
 parser.add_argument("--out_file", default=None, help="file to write issue layer output")
 parser.add_argument("--issue_type", dest="issue_types", action='append', default=None,
@@ -30,10 +30,13 @@ def main():
         map_dir = os.path.join(os.path.expanduser('~'), 'tiled_maps/usa')
 
     map_reader_dir = os.path.join(map_dir, 'map_reader')
-    route_ids = set()
-    for fn in glob.glob(os.path.join(map_reader_dir, '*.json')):
-        route_id = os.path.splitext(os.path.basename(fn))[0]
-        route_ids.add(route_id)
+    if args.route_ids:
+        route_ids = set(args.route_ids)
+    else:
+        route_ids = set()
+        for fn in glob.glob(os.path.join(map_reader_dir, '*.json')):
+            route_id = os.path.splitext(os.path.basename(fn))[0]
+            route_ids.add(route_id)
 
     issue_layer, failures = route_linter.lint_routes(map_dir, map_reader_dir, route_ids, args.issue_types)
 
