37a753bd176382a58107b7bc64ef6369cdffd863
Sam Kunz <sam@embarktrucks.com>
2020-05-08 14:50:40 -0700

    change setup.py version to 0.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_requirements():
 
 setuptools.setup(
     name="embark-pymaputils",
-    version="0.0.1",
+    version="0.0.2",
     description="Embark Python Map Utils",
     url="https://github.com/embarktrucks/pymaputils",
     packages=setuptools.find_packages('src'),
