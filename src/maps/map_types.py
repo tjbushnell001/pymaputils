836537fabb8efcfce3eddd5cd8d81445b5fd2916
Sam Kunz <sam@embarktrucks.com>
2020-06-19 16:07:56 -0700

    add LaneAnnotationLayerName strings

--- src/maps/map_types.py
+++ src/maps/map_types.py
@@ -12,3 +12,11 @@ class MapType(Enum):
     DISENGAGE_ZONE = "DISENGAGE_ZONE"
     LIDAR_LINE = "LIDAR_LINE"
     LANE_PREFERENCE = "LANE_PREFERENCE"
+
+
+class LaneAnnotationLayerName(object):
+    NO_PASSING_ZONES = 'no_passing_zones'
+    DISENGAGE_ZONES = 'disengage_zones'
+    DISENGAGE_ZONES_LTG = 'disengage_zones_ltg'
+    UNDRIVABLE_RIGHT_SHOULDER_ZONES = 'undrivable_right_shoulder_zones'
+    ROAD_MARKING_ZONES = 'road_marking_zones'
