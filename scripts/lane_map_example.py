720c00816e0f1c7da99bf4075d59b6b454c264a3
Christian <cjwawrzonek@gmail.com>
2020-08-24 15:44:57 -0700

    pylint

--- scripts/lane_map_example.py
+++ scripts/lane_map_example.py
@@ -8,12 +8,12 @@ from maps.utils import tile_utils
 
 
 def parse_args(args=None):
-  parser = argparse.ArgumentParser(description="Example lane map data parsing script to find lane group tha intersects a point.")
-  parser.add_argument('--map_dir', type=str, default="~/tiled_maps",
-                      help="The location of the tiled_maps directory. Defaults to embite storage location.")
-  parser.add_argument('--gps_lat', type=float, default=34.02034454, help="gps latitude point to search for intersection.")
-  parser.add_argument('--gps_lon', type=float, default=-117.101569874, help="gps longitude point to search for intersection.")
-  return parser.parse_args(args=args)
+	parser = argparse.ArgumentParser(description="Example lane map data parsing script to find lane group tha intersects a point.")
+	parser.add_argument('--map_dir', type=str, default="~/tiled_maps",
+						help="The location of the tiled_maps directory. Defaults to embite storage location.")
+	parser.add_argument('--gps_lat', type=float, default=34.02034454, help="gps latitude point to search for intersection.")
+	parser.add_argument('--gps_lon', type=float, default=-117.101569874, help="gps longitude point to search for intersection.")
+	return parser.parse_args(args=args)
 
 
 def main(args):
@@ -42,14 +42,14 @@ def main(args):
 	for lane_group in lane_groups.values():
 		polygon = sg.Polygon(lane_group.geometry.coordinates[0])
 		if sg.Point(GPS_POINT).intersects(polygon):
+			# Here are all the features of the lane group you fetched!
+			print '-------------- Properties --------------'
+			print lane_group.properties
+			print '----------------------------------------'
+			print 'is highway:', lane_group.properties['is_controlled_access']
+			print 'is ramp:', lane_group.properties['is_ramp']
 			break
 
-	# Here are all the features of the lane group you fetched!
-	print '-------------- Properties --------------'
-	print lane_group.properties
-	print '----------------------------------------'
-	print 'is highway:', lane_group.properties['is_controlled_access']
-	print 'is ramp:', lane_group.properties['is_ramp']
 
 if __name__ == "__main__":
 	args = parse_args()
